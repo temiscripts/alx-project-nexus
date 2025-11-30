2b5ee5662532b22d72984d0c8c1626b4b275a745
temiscripts <mokhefue@gmail.com>
2025-11-30 14:58:38 +0100

    feat: add createUser mutation

--- core/schema.py
+++ core/schema.py
@@ -21,7 +21,25 @@ class Query(graphene.ObjectType):
     def resolve_users(self, info):
         return User.objects.all()
 
+class CreateUser(graphene.Mutation):
+    user = graphene.Field(UserType)
+
+    class Arguments:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+        email = graphene.String(required=True)
+
+    def mutate(self, info, username, password, email):
+        user = User(
+            username=username,
+            email=email,
+        )
+        user.set_password(password)
+        user.save()
+        return CreateUser(user=user)
+
 class Mutation(graphene.ObjectType):
+    create_user = CreateUser.Field()
     token_auth = graphql_jwt.ObtainJSONWebToken.Field()
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
